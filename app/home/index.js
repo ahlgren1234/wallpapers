153766b8a6e6e8eb43ddd1eead18e6b221d1cf55
Peter Ahlgren <ahlgren1234@gmail.com>
2025-01-23 15:54:21 +0100

    search and categories

--- app/home/index.js
+++ app/home/index.js
@@ -1,15 +1,77 @@
 import { View, Text, StyleSheet, Pressable, ScrollView, TextInput } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { Feather, FontAwesome6, Ionicons } from '@expo/vector-icons';
 import { theme } from '../../constants/theme';
 import { hp, wp } from '../../helpers/common';
+import Categories from '../../components/categories';
+import { apiCall } from '../../api';
+import ImageGrid from '../../components/imageGrid';
+import { debounce } from 'lodash';
 
+var page = 1;
 
 const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
     const paddingTop = top > 0 ? top + 10 : 30;
+    const [search, setSearch] = useState('');
+    const [images, setImages] = useState([]);
+    const [activeCategory, setActiveCategory] = useState(null);
+    const searchInputRef = useRef(null);
+
+    useEffect(() => { 
+        fetchImages();
+    }, [])
+
+    const fetchImages = async (params = { page: 1 }, append = false) => {
+        let res = await apiCall(params);
+        if (res.success && res?.data?.hits) {
+            if (append)
+                setImages([...images, ...res.data.hits])
+            else
+                setImages([...res.data.hits])
+        }
+    }
+
+    const handleChangeCategory = (cat) => {
+        setActiveCategory(cat);
+        clearSearch();
+        setImages([]);
+        page = 1;
+        let params = {
+            page,
+        }
+        if (cat) params.category = cat;
+        fetchImages(params, false);
+    }
+
+    const handleSearch = (text) => {
+        setSearch(text);
+        if (text.length > 2) {
+            // search for this text
+            page = 1;
+            setImages([]);
+            setActiveCategory(null);
+            fetchImages({page, q: text}, false)
+        }
+
+        if (text == "") {
+            // reset results
+            page = 1;
+            searchInputRef?.current?.clear();
+            setImages([]);
+            setActiveCategory(null);
+            fetchImages({ page }, false);
+        }
+    }
+
+    const clearSearch = () => {
+        setSearch('');
+        searchInputRef?.current?.clear();
+    }
+
+    const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
 
     return (
         <View style={[styles.container, {paddingTop}]}>
@@ -35,12 +97,33 @@ const HomeScreen = () => {
                     </View>
                     <TextInput
                         placeholder='Search for photos...'
+                        // value={search}
+                        ref = {searchInputRef}
+                        onChangeText={handleTextDebounce}
                         style={styles.searchInput}
                     />
-                    <Pressable style={styles.closeIcon}>
-                        <Ionicons name="close" size={24} color={theme.colors.neautral(0.6)} />
-                    </Pressable>
+                    { 
+                        search && (
+                            <Pressable onPress={() => handleSearch("")} style={styles.closeIcon}>
+                                <Ionicons name="close" size={24} color={theme.colors.neautral(0.6)} />
+                            </Pressable>
+                        )
+                    }
+                    
                 </View>
+
+                {/* categories */}
+                <View style={styles.categories}>
+                    <Categories activeCategory={activeCategory} handleChangeCategory={handleChangeCategory} />
+                </View>
+
+                {/* images masonry grid */}
+                <View>
+                    { 
+                        images.length > 0 && <ImageGrid images={ images} />
+                    }
+                </View>
+
             </ScrollView>
         </View>
     )
@@ -61,6 +144,32 @@ const styles = StyleSheet.create({
         fontSize: hp(4),
         fontWeight: theme.fontWeights.semibold,
         color: theme.colors.neautral(0.9),
+    },
+    searchBar: {
+        marginHorizontal: wp(4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.colors.grayBG,
+        backgroundColor: theme.colors.white,
+        padding: 6,
+        paddingLeft: 10,
+        borderRadius: theme.radius.lg,
+    },
+    searchIcon: {
+        padding: 8,
+    },
+    searchInput: {
+        flex: 1,
+        borderRadius: theme.radius.sm,
+        paddingVertical: 10,
+        fontSize: hp(1.8),
+    },
+    closeIcon: {
+        backgroundColor: theme.colors.neautral(0.1),
+        padding: 8,
+        borderRadius: theme.radius.sm,
     },
 })
 
